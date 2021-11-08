ecb2b7f1cb948e7280d5a9942856c5a918c2d9f7
Tim Zhu <53026628+lodgepolepines@users.noreply.github.com>
2021-11-08 18:20:15 -0500

    Update local.py

--- local.py
+++ local.py
@@ -3,12 +3,16 @@ from upload import ABUploader
 load_dotenv()
 # Change these variables.
 
-import requests
+import request
 import csv
-url = 'https://raw.githubusercontent.com/lodgepolepines/ab-uploader-cwa/main/ab_uploader_test.csv'
-r = requests.get(url)
-text = r.iter_lines()
-upload_file = csv.reader(text, delimiter=',')
+# Define the remote URL
+url = "https://raw.githubusercontent.com/lodgepolepines/ab-uploader-cwa/main/ab_uploader_test.csv"
+# Send HTTP GET request via requests
+data = requests.get(url)
+# Convert to iterator by splitting on \n chars
+lines = data.text.splitlines()
+# Parse as CSV object
+upload_file = csv.reader(lines)
 
 config_file = 'config.example.yml'
 campaign_key = 'upload-test'
